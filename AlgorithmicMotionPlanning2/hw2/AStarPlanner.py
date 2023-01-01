4ab107a882861396b17b530d2b21e78915dc26a4
texydo <30366698+texydo@users.noreply.github.com>
2023-01-01 12:13:35 +0200

    Add files via upload
    organized astar bit more

--- AlgorithmicMotionPlanning2/hw2/AStarPlanner.py
+++ AlgorithmicMotionPlanning2/hw2/AStarPlanner.py
@@ -1,10 +1,11 @@
 import numpy as np
+import heapq
 
 
 class AStarPlanner(object):
     def __init__(self, planning_env):
         self.planning_env = planning_env
-        self.epsilon = 10
+        self.epsilon = 20
 
         # used for visualizing the expanded nodes
         # make sure that this structure will contain a list of positions (states, numpy arrays) without duplicates
@@ -24,7 +25,7 @@ class AStarPlanner(object):
 
     def plan(self):
         '''
-        Compute and return the plan. The function should return a numpy array containing the states (positions) of the robot.
+            Compute and return the plan. The function should return a numpy array containing the states (positions) of the robot.
         '''
 
         # initialize an empty plan.
@@ -32,87 +33,66 @@ class AStarPlanner(object):
 
         # TODO: Task 4.3
         possible_movements = [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-        start = self.planning_env.start
-        end = self.planning_env.goal
+        start = tuple(self.planning_env.start)
+        end = tuple(self.planning_env.goal)
         start_node = self.Node(None, start)
         start_node.g = start_node.h = start_node.f = 0
-        end_node = self.Node(None, end)
-        end_node.g = end_node.h = end_node.f = 0
 
-        open_list = []
-        closed_list = []
+        open_dict = {}
+        closed_dict = {}
+        open_dict[start] = start_node
 
-        # Add the start node
-        open_list.append(start_node)
+        open_f_heap = []
+        heapq.heappush(open_f_heap, (0, start))
 
-        # Loop until you find the end
-        while len(open_list) > 0:
-            # Get the current node
-            current_node = open_list[0]
-            current_index = 0
-            for index, item in enumerate(open_list):
-                if item.f < current_node.f:
-                    current_node = item
-                    current_index = index
-
-            # Pop current off open list, add to closed list
-            open_list.pop(current_index)
-            closed_list.append(current_node)
-            self.expanded_nodes.append(current_node.position)
-
-            # Found the goal
-            if current_node == end_node:
+        while len(open_dict) > 0:
+            current_point = heapq.heappop(open_f_heap)[1]
+            if current_point in closed_dict:
+                continue
+            closed_dict[current_point] = open_dict[current_point]
+            del open_dict[current_point]
+            self.expanded_nodes.append(current_point)
+            if current_point == end:
                 path = []
-                current = current_node
+                current = closed_dict[current_point]
+                path_cost = 0
                 while current is not None:
                     path.append(np.array(current.position))
+                    if current.parent is not None:
+                        path_cost += self.planning_env.compute_distance(np.array(current.parent.position),
+                                                                        np.array(current.position))
                     current = current.parent
-                return np.array(path[::-1])  # Return reversed path
-
-            # Generate children
-            children = []
+                print("Path cost:")
+                print(path_cost)
+                print("Number of states expanded")
+                print(len(closed_dict))
+                return np.array(path[::-1])
+            current_node = closed_dict[current_point]
             for new_position in possible_movements:  # Adjacent squares
                 # Get node position
-                node_position = [current_node.position[0] + new_position[0], current_node.position[1] + new_position[1]]
+                node_position = [current_point[0] + new_position[0], current_point[1] + new_position[1]]
 
                 # Make sure within range
                 if not self.planning_env.state_validity_checker(node_position):
                     continue
-
                 # Make sure walkable terrain
-                if not self.planning_env.edge_validity_checker(current_node.position, node_position):
+                if not self.planning_env.edge_validity_checker(current_point, node_position):
                     continue
 
                 # Create new node
-                new_node = self.Node(current_node, node_position)
-
-                # Append
-                children.append(new_node)
-
-            # Loop through children
-            for child in children:
-                # Child is on the closed list
-                for closed_child in closed_list:
-                    if child == closed_child:
-                        continue
-
-                # Create the f, g, and h values
-                child.g = child.parent.g + self.planning_env.compute_distance(child.parent.position, child.position)
-                child.h = self.planning_env.compute_heuristic(child.position)
+                child = self.Node(current_node, tuple(node_position))
+                if child.position in closed_dict:
+                    continue
+                child.g = child.parent.g + self.planning_env.compute_distance(np.array(child.parent.position), node_position)
+                child.h = self.planning_env.compute_heuristic(node_position)
                 child.f = child.g + self.epsilon * child.h
-
-                # Child is already in the open list
-                pop_list = []
-                for index, open_node in enumerate(open_list):
-                    if child == open_node:
-                        if child.g > open_node.g:
-                            continue
-                        else:
-                            pop_list.append(index)
-                for index in sorted(pop_list, reverse=True):
-                    del open_list[index]
-                # Add the child to the open list
-                open_list.append(child)
+                if child.position in open_dict:
+                    if child.g > open_dict[child.position].g:
+                        continue
+                    else:
+                        del open_dict[child.position]
+                open_dict[child.position] = child
+                heapq.heappush(open_f_heap, (child.f, child.position))
         return None
 
     def get_expanded_nodes(self):
